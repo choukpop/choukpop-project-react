56b11a1c0d0f6249a8620b153c330403fc6d1d26
choukpop <choukpop1337@gmail.com>
2023-04-12 21:15:04 +0100

    add footbar

---
+++ src/components/genshin/Footbar.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function Footbar() {
+
+    return (
+        <div className="p-4 text-white text-2xl">
+            By ChouKpop
+        </div>
+    )
+}
+
+export default Footbar
